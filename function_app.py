c6ea11a0e1631e5fc92e1e43a21421b0c8ec414e
wesleycmpsilva <121673786+wesleycmpsilva@users.noreply.github.com>
2024-02-06 22:59:49 +0000

    test

--- function_app.py
+++ function_app.py
@@ -17,57 +17,6 @@ url_aurora = [
     {'sys_name': 'STP', 'client': '100', 'url': 'http://spstptmsa001.one.ofc.loc:8000', 'alias': 'https://tm.stout.aurora.ab-inbev.com', 'akvkey': 'sysmicsautopass-stp100'},
 ]
 
-def get_auth_headers2(item, system_name):
-    headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
-
-    count_success = 0
-    output = ""  # Initialize an empty string to accumulate messages
-
-    # Loop through each URL and make a request with its corresponding password
-    url = item['url']
-    alias = item['alias']
-    passwordKey = item['akvkey']
-    client = item['client']
-
-    service_url = url + f"/sap/opu/odata/AB4/GRC_CTE_OD_SEC01_APP_SRV_01/ControlDetailSet?sap-client={client}&sap-language=EN&saml2=disabled&$format=json"
-    service_alias = alias + f"/sap/opu/odata/AB4/GRC_CTE_OD_SEC01_APP_SRV_01/ControlDetailSet?sap-client={client}&sap-language=EN&saml2=disabled&$format=json"
-
-    user = 'SYS_MICSAUTO'
-    password = os.environ['AURORA_PASSWORD'] 
-    auth = HTTPBasicAuth(username=user, password=password)
-
-    exception_bool = False
-    try:
-        response = requests.get(service_url, headers=headers, auth=auth, verify=True, timeout=5)
-        output += f"{response.status_code} for {url}\n"
-        output += response.text[:150] + "\n"
-        response = requests.get(service_url, headers=headers, auth=auth, verify=False, timeout=5)
-        output += f"{response.status_code} for {url}\n"
-        output += response.text[:150] + "\n"
-
-        exception_bool = True
-    except Exception as e:
-        output += f"Error for {url}: {str(e)[0:75]}\n"
-
-    try:
-        response = requests.get(service_alias, headers=headers, auth=auth, verify=True, timeout=5)
-        output += f"{response.status_code} for {alias}\n"
-        output += response.text[:150] + "\n"
-        response = requests.get(service_alias, headers=headers, auth=auth, verify=False, timeout=5)
-        output += f"{response.status_code} for {alias}\n"
-        output += response.text[:150] + "\n"
-
-        exception_bool = True
-    except Exception as e:
-        output += f"Error for {alias}: {str(e)[0:75]}\n"
-        output += "{Erro by calling " + url + "}\n"
-
-    if exception_bool:
-        count_success += 1
-    output += "\n\n"
-
-    return output 
-
 @app.route(route="http_adb_middleware")
 def http_adb_middleware(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
