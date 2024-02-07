c4a03682708d4d04d8ce2b22e363916cc0894232
wesleycmpsilva <121673786+wesleycmpsilva@users.noreply.github.com>
2024-02-07 00:26:31 +0000

    details + action

--- function_app.py
+++ function_app.py
@@ -17,54 +17,28 @@ url_aurora = [
     {'sys_name': 'STP', 'client': '100', 'url': 'http://spstptmsa001.one.ofc.loc:8000', 'alias': 'https://tm.stout.aurora.ab-inbev.com', 'akvkey': 'AURORA_PASSWORD'},
 ]
 
-def get_auth_headers(item):
+def https_call(item):
     headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
 
     count_success = 0
-    output = ""  # Initialize an empty string to accumulate messages
+    output = "" # Initialize an empty string to accumulate messages
 
     # Loop through each URL and make a request with its corresponding password
     url = item['url']
-    alias = item['alias']
+    user = item['username']
     passwordKey = item['akvkey']
-    client = item['client']
-
-    service_url = url + f"/sap/opu/odata/AB4/GRC_CTE_OD_SEC01_APP_SRV_01/ControlDetailSet?sap-client={client}&sap-language=EN&saml2=disabled&$format=json"
-    service_alias = alias + f"/sap/opu/odata/AB4/GRC_CTE_OD_SEC01_APP_SRV_01/ControlDetailSet?sap-client={client}&sap-language=EN&saml2=disabled&$format=json"
-
-    user = 'SYS_MICSAUTO'
+    ssl = item['ssl']
+    
     password = os.environ[passwordKey] 
     auth = HTTPBasicAuth(username=user, password=password)
 
-    exception_bool = False
     try:
-        response = requests.get(service_url, headers=headers, auth=auth, verify=True, timeout=5)
-        output += f"{response.status_code} for {url}\n"
-        output += response.text[:150] + "\n"
-        response = requests.get(service_url, headers=headers, auth=auth, verify=False, timeout=5)
+        response = requests.get(service_url, headers=headers, auth=auth, verify=ssl, timeout=5)
         output += f"{response.status_code} for {url}\n"
         output += response.text[:150] + "\n"
 
-        exception_bool = True
     except Exception as e:
-        output += f"Error for {url}: {str(e)[0:75]}\n"
-
-    try:
-        response = requests.get(service_alias, headers=headers, auth=auth, verify=True, timeout=5)
-        output += f"{response.status_code} for {alias}\n"
-        output += response.text[:150] + "\n"
-        response = requests.get(service_alias, headers=headers, auth=auth, verify=False, timeout=5)
-        output += f"{response.status_code} for {alias}\n"
-        output += response.text[:150] + "\n"
-
-        exception_bool = True
-    except Exception as e:
-        output += f"Error for {alias}: {str(e)[0:75]}\n"
         output += "{Erro by calling " + url + "}\n"
-
-    if exception_bool:
-        count_success += 1
-    output += "\n\n"
 
     return output
 
@@ -72,25 +46,20 @@ def get_auth_headers(item):
 def http_adb_middleware(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    pos = req.params.get('pos')
-    if not pos:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            pos = req_body.get('pos')
-
-    if not pos:
+    try:
+        req_body = req.get_json()
+    except ValueError:
         return func.HttpResponse(f"You are not calling any request")
 
-    pos = int(pos)
-    if pos == -1:
-        return func.HttpResponse(f"You are calling the request {pos}")
-    if pos >= 0:
-        output = get_auth_headers(url_aurora[pos])
+    action = req_body.get('action')
+    details = req_body.get('details')
 
-        return func.HttpResponse(
-             output + "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    if action == 0:
+        output = https_call(details)
+    #elif action == 1:
+    #    output = sql_vm(req_body)
+
+    return func.HttpResponse(
+            output + "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
+    )
