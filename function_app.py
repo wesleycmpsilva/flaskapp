726ec221509929d8ef56324d11a0c061cc8bd0cb
wesleycmpsilva <121673786+wesleycmpsilva@users.noreply.github.com>
2024-02-07 01:09:28 +0000

    error

--- function_app.py
+++ function_app.py
@@ -48,7 +48,7 @@ def https_call(item):
         output += response.text[:150] + "\n"
 
     except Exception as e:
-        output += "{Erro by calling " + url + route + "}\n"
+        output += f"Erro {e} by calling + {url}{route} \n"
 
     return output
 
