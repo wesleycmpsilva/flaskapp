316e4e34a7e24a7b104739760ac4d099d8e1a4e5
wesleycmpsilva <121673786+wesleycmpsilva@users.noreply.github.com>
2024-02-07 20:36:05 +0000

    sql vm func

--- function_app.py
+++ function_app.py
@@ -4,18 +4,9 @@ import requests
 from pprint import pprint
 from requests.auth import HTTPBasicAuth
 import os
+import pyodbc
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-
-url_aurora = [
-    {'sys_name': 'GAP', 'client': '100', 'url': 'https://solman.ab-inbev.com', 'alias': 'https://solman.ab-inbev.com', 'akvkey': 'AURORA_PASSWORD'},
-    {'sys_name': 'GCP', 'client': '100', 'url': 'http://spgbpgtsa001.one.ofc.loc:8000', 'alias': 'https://slt.aurora.ab-inbev.com', 'akvkey': 'AURORA_PASSWORD'},
-    {'sys_name': 'GWP', 'client': '200', 'url': 'http://spgbpsbwa001.one.ofc.loc:8000', 'alias': 'https://bw.aurora.ab-inbev.com', 'akvkey': 'AURORA_PASSWORD'},
-    {'sys_name': 'S4P', 'client': '100', 'url': 'http://spstpss4a001.one.ofc.loc:8000', 'alias': 'https://s4.stout.aurora.ab-inbev.com', 'akvkey': 'AURORA_PASSWORD'},
-    {'sys_name': 'S7P', 'client': '100', 'url': 'http://spstptdfa001.one.ofc.loc:8000', 'alias': 'https://tdf.stout.aurora.ab-inbev.com', 'akvkey': 'AURORA_PASSWORD'},
-    {'sys_name': 'S9P', 'client': '100', 'url': 'https://sigga.stout.aurora.ab-inbev.com', 'alias': 'https://sigga.stout.aurora.ab-inbev.com', 'akvkey': 'AURORA_PASSWORD'},
-    {'sys_name': 'STP', 'client': '100', 'url': 'http://spstptmsa001.one.ofc.loc:8000', 'alias': 'https://tm.stout.aurora.ab-inbev.com', 'akvkey': 'AURORA_PASSWORD'},
-]
 
 def str_to_bool(s):
     # Convert the string to lowercase to make the function case-insensitive
@@ -52,6 +43,38 @@ def https_call(item):
 
     return output
 
+def sql_vm(item):
+
+    ip = item['ip']
+    database = item['database']
+    user = item['username']
+    passwordKey = item['akvkey']
+
+    connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={ip};DATABASE={database};UID={user};PWD={passwordKey};Authentication=ActiveDirectoryPassword;TrustServerCertificate=YES;'
+    
+    try:
+        conn = pyodbc.connect(connectionString)
+
+        SQL_QUERY = """SELECT 1"""
+        cursor = conn.cursor()
+        cursor.execute(SQL_QUERY)
+        
+        columns = [column[0] for column in cursor.description]
+        results = []
+
+        rows = cursor.fetchall()
+        conn.close()
+        for row in rows:
+            results.append(dict(zip(columns, row)))
+
+    except Exception as e:
+        try:
+            driver = sorted(pyodbc.drivers()).pop()
+        except:
+            return f"Error {e} \n Here we have only"
+        return f"Error {e} \n Here we have only {driver} \n {query}"
+    return results
+
 @app.route(route="http_adb_middleware")
 def http_adb_middleware(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -66,8 +89,8 @@ def http_adb_middleware(req: func.HttpRequest) -> func.HttpResponse:
 
     if action == 0:
         output = https_call(details)
-    #elif action == 1:
-    #    output = sql_vm(req_body)
+    elif action == 1:
+        output = sql_vm(details)
 
     return func.HttpResponse(
             output + "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
